72b5d77a0e3648478dc0f2ee3a56a886c744f996
Adam Vollrath <adam@endpoint.com>
2012-12-10 18:24:47 -0500

    A very simple functional test for Auth.User admin interface.

--- lg_cms/functional_tests/tests.py
+++ lg_cms/functional_tests/tests.py
@@ -6,11 +6,13 @@
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Functional tests are grouped into classes, and each test is a method inside
 # the class. The special rule is that test methods must begin with a test_.
 
-class FileTest(LiveServerTestCase):
+class UserTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
 
 # The special methods setUp and tearDown are executed before and after each
 # test. We're using them to start up and shut down our Selenium WebDriver
@@ -28,10 +30,10 @@ class FileTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_create_new_asset_via_admin_site(self):
+    def test_can_create_new_user_via_admin_site(self):
         ## User opens their web browser, and goes to the admin page.
-        #self.browser.get(self.live_server_url + '/admin/')
-        self.browser.get('http://10.42.41.1/admin/')
+        self.browser.get(self.live_server_url + '/admin/')
+        #self.browser.get('http://10.42.41.1/cms/admin/')
         # .get is tells the browser to go to a new page, and we pass it the
         # url, which is made up of self.live_server_url, which is set up for us
         # by LiveServerTestCase, and then we tack on the /admin/ url to get to
@@ -48,5 +50,66 @@ class FileTest(LiveServerTestCase):
         # Finally, we get to an assertion - where we say what we expect,
         # and the test should pass or fail at this point.
 
-        # TODO: use the admin site to create an asset
-        self.fail('finish this test!')
+        # The user types in her username and password and hits "Return".
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('galadmin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('galadmin')
+        password_field.send_keys(Keys.RETURN)
+
+        # The username and password should be accepted, and the user taken
+        # to the Site Administration page.
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        # The user now sees a couple of links for the "Auth" application.
+        user_links = self.browser.find_elements_by_link_text("Users")
+        self.assertEquals(len(user_links), 1)
+
+        group_links = self.browser.find_elements_by_link_text("Groups")
+        self.assertEquals(len(group_links), 1)
+
+        # The user clicks the "Users" link to list the current users.
+        user_links[0].click()
+
+        body = self.browser.find_element_by_tag_name('body')
+
+        # The user is taken to the User listing page, which shows only one user.
+        self.assertIn('1 user', body.text)
+
+        # There's a link to 'add' a new User, so the user clicks it.
+        new_user_link = self.browser.find_element_by_link_text('Add user')
+        new_user_link.click()
+
+        # The user sees some input fields for "Username" and "Password".
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Username:', body.text)
+        self.assertIn('Password:', body.text)
+
+        # The user identifies the fields to create a new user.
+        username_field = self.browser.find_element_by_name('username')
+        password1_field = self.browser.find_element_by_name('password1')
+        password2_field = self.browser.find_element_by_name('password2')
+
+        # The user enters a new username and password (twice).
+        username_field.send_keys('foo')
+        password1_field.send_keys('bar')
+        password2_field.send_keys('bar')
+
+        # Now click the "Save" link
+        self.browser.find_element_by_css_selector("input[value='Save']").click()
+
+        # There's a lot more options here, but we'll just save as-is.
+        self.browser.find_element_by_name("_save").click()
+ 
+        # Now we're back at the User listing page.  There should be two...
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('2 users', body.text)
+
+        # ... including the new one.
+        new_user_links = self.browser.find_elements_by_link_text('foo')
+        self.assertEquals(len(new_user_links), 1)
+
+        # Log out.
+        self.browser.find_element_by_link_text('Log out')
