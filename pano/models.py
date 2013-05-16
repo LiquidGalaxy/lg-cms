ec3e3d737ad44c1d6103fe3cf37fb9079d074e13
Adam Vollrath <adam@endpoint.com>
2013-05-16 17:46:59 -0400

    Template and URL configuration for krpano XML integration

--- pano/models.py
+++ pano/models.py
@@ -13,11 +13,11 @@ class Panorama(Item):
     projection = models.TextField(choices=KRPANO_PROJECTION_CHOICES)
 
     # http://krpano.com/docu/xml/#image.hfov
-    hfov = models.DecimalField(blank=True, default=360.0,
+    hfov = models.DecimalField(blank=True, max_digits=5, decimal_places=2, default=360.0,
         help_text="""Defines the horizontal field of view (hfov) of the pano image in degrees. This is the visible range that was captured on the pano image. The default value is 360, which means a view all around. Use a smaller value for partial panos. For Flat panos (or normal images) the value "1.0" should be used. """)
-    vfov = models.DecimalField(blank=True, # default=180.0,
+    vfov = models.DecimalField(blank=True, max_digits=5, decimal_places=2, # default=180.0,
         help_text="""Defines the vertical field of view (vfov) of the pano image in degrees. By default (when no value was set), this value will be calculated automatically by using the hfov, the type of the pano and the side aspect of the pano image.""")
-    voffset = models.DecimalField(blank=True, # default=0.0,
+    voffset = models.DecimalField(blank=True, max_digits=5, decimal_places=2, # default=0.0,
         help_text="""Defines the vertical offset of the pano image in degrees. By default the pano image will be centered in the 3D space. This means the viewing range will be from -hfov/2 to +hfov/2 and from -vfov/2 to +vfov/2. Now when using a partial pano image where the horizon is not in the middle of image (not at 0 degree), then this image will be displayed distorted. The voffset can be used to shift the fov range up or down to center the image correctly in the 3D space.""")
 
     group = models.ForeignKey('PanoramaGroup')
