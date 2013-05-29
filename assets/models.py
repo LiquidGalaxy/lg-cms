cabcc48f6acc47f2d917311b4a9f56a49e190baa
Adam Vollrath <adam@endpoint.com>
2013-05-28 20:29:27 -0400

    Better handle asset filenames with unrecognized extensions.
    If the MIME type cannot be guessed, demand an explicit one.

--- assets/models.py
+++ assets/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 from mimetypes import guess_type
+
+# For upload and file creation times.
+from django.utils import timezone
 
 # Create your models here.
 
@@ -17,12 +21,24 @@ class Item(models.Model):
     update_time = models.DateTimeField(auto_now=True)
 
     storage = models.FileField(upload_to='asset_storage', max_length=250)
-    mime_type = models.CharField(max_length=80, blank=True)
+    mime_type = models.CharField(max_length=80, blank=True, default=None)
 
-    def save(self, *args, **kwargs):
-        if self.storage.url is not '':
-            self.mime_type = guess_type(self.storage.url)[0]
-        super(Item, self).save(*args, **kwargs) # Call the "real" save() method.
+    def clean(self):
+        # Populate the Creation Time.
+        if self.creation_time is None:
+            self.creation_time = timezone.now()
+        # Guess the MIME type if none is submitted.
+        if not self.mime_type:
+            try:
+                guessed_mime_type = guess_type(self.storage.path)[0]
+            except ValueError:
+                raise ValidationError('Provide asset file.')
+            if guessed_mime_type is None: # If the MIME type cannot be guessed,
+                raise ValidationError(
+                    'Could not recognize file extension type. ' +
+                    'Please change filename or specify MIME type.')
+            else:
+                self.mime_type = guessed_mime_type
 
     def __unicode__(self):
         return self.title
--- assets/tests.py
+++ assets/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
-from django.utils import timezone
 from assets.models import Item
 
 # To test unique constraints.
 from django.db.utils import IntegrityError
+# To test input validation.
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+#from datetime import datetime
 
 class ItemUploadTest(TestCase):
     def test_creating_a_new_item_with_a_new_file(self):
@@ -14,9 +17,20 @@ class ItemUploadTest(TestCase):
         item.slug = "foo-item"
         item.description = "Moar foo than yoo."
 
+        # Confirm that we're really testing the .clean() method.
+        self.assertEquals(item.creation_time, None)
+
+        # Trying to clean the object before specifying a file should handle
+        #  the storage.path ValueError gracefully and raise ValidationError.
+        self.assertRaises(ValidationError, item.clean)
+
         item.storage = 'foo.txt'
 
-        item.creation_time = timezone.now()
+        # Now that we've a filename specified,
+        # The Creation Time and MIME should be filled in by the clean() method.
+        item.clean()
+        self.assertIsInstance(item.creation_time, type(timezone.now())) # close
+        self.assertEquals(item.mime_type, 'text/plain')
         
         # Check that we can save it in the database.
         item.save()
@@ -36,6 +50,56 @@ class ItemUploadTest(TestCase):
         # And test that the MIME type was correctly determined.
         self.assertEquals(my_item_in_database.mime_type, 'text/plain')
 
+    def test_creating_an_item_with_unrecognized_mime_type(self):
+        """Don't accept files without a recognized MIME Type."""
+        # Start by creating a new Item object
+        item = Item()
+        
+        item.title = "Bar Item"
+        item.slug = "bar-item"
+        item.description = "File with unrecognized extension and unknown MIME."
+        item.creation_time = timezone.now()
+
+        # This filename with '.foo' extension is not a recognized MIME type.
+        # TODO: Look for magic numbers etc. to determine MIME type?
+        item.storage = 'deleteme.foo'
+
+        # The .clean() method will guess the MIME type.
+        # This should raise a ValidationError if it cannot guess.
+        self.assertRaises(ValidationError, item.clean)
+
+        # Trying to save this unclean object should be prevented by the DB.
+        self.assertRaises(IntegrityError, item.save)
+
+        # Specifying a blank MIME type should also raise an exception.
+        item.mime_type = ''
+
+        # Trying to save this file should raise a ValidationError.
+        self.assertRaises(ValidationError, item.clean)
+
+        # Now "manually" specify a valid MIME type.
+        item.mime_type = 'multipart/encrypted'
+
+        # This should allow it to clean and save normally.
+        item.clean()
+        item.save()
+
+        my_id = item.id
+
+        # Now check we can find it in the database again.
+        my_item_in_database = Item.objects.get(id=my_id)
+        self.assertEquals(my_item_in_database, item)
+
+        # And check that it's properly saved its attributes
+        self.assertEquals(my_item_in_database.title, "Bar Item")
+        self.assertEquals(my_item_in_database.slug, "bar-item")
+        self.assertEquals(my_item_in_database.description,
+            "File with unrecognized extension and unknown MIME.")
+        self.assertEquals(my_item_in_database.creation_time, item.creation_time)
+
+        # And test that the MIME type was correctly written.
+        self.assertEquals(my_item_in_database.mime_type, 'multipart/encrypted')
+
     def test_duplicate_slugs(self):
         """ Test that saving Items with duplicate slugs causes an Exception. """
 
@@ -48,6 +112,7 @@ class ItemUploadTest(TestCase):
 
         item1.storage = 'foo.txt'
 
+        item1.clean()
         item1.save()
 
         # Create and save the second object.
@@ -59,6 +124,7 @@ class ItemUploadTest(TestCase):
 
         item2.storage = 'foo.txt'
 
+        item2.clean()
         # Trying to save a duplicate item should raise an Integrity Exception.
         self.assertRaisesRegexp(item2.save, IntegrityError)
 
